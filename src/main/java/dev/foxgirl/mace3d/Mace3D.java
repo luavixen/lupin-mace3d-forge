55a795595bcc05cc36df4fc6115ed00a872902ae
Lua MacDougall <lua@foxgirl.dev>
2024-08-03 12:49:10 -0700

    Update w/ changes from main project, switch to submodule

--- src/main/java/dev/foxgirl/mace3d/Mace3D.java
+++ src/main/java/dev/foxgirl/mace3d/Mace3D.java
@@ -1,11 +1,1 @@
-package dev.foxgirl.mace3d;
-
-import net.minecraft.client.resources.model.ModelResourceLocation;
-import net.minecraft.resources.ResourceLocation;
-
-public final class Mace3D {
-
-    public static final ModelResourceLocation MACE = ModelResourceLocation.inventory(ResourceLocation.withDefaultNamespace("mace"));
-    public static final ModelResourceLocation MACE_IN_HAND = ModelResourceLocation.inventory(ResourceLocation.withDefaultNamespace("mace_in_hand"));
-
-}
+../../../../../../lupin-mace3d/common/src/main/java/dev/foxgirl/mace3d/Mace3D.java
--- src/main/java/dev/foxgirl/mace3d/Mace3DMod.java
+++ src/main/java/dev/foxgirl/mace3d/Mace3DMod.java
@@ -1,13 +1,8 @@
 package dev.foxgirl.mace3d;
 
 import net.minecraftforge.fml.common.Mod;
-import org.apache.logging.log4j.LogManager;
 
 @Mod("mace3d")
 public final class Mace3DMod {
-
-    public Mace3DMod() {
-        LogManager.getLogger("mace3d").info("Mace3D loading...");
-    }
 
 }
